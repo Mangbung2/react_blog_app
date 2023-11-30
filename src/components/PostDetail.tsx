0385b0b9e6692cd2d24732fd1a5ebd953dcff01d
[StarIs] <[diablo8273@gmail.com]>
2023-11-30 23:49:04 +0900

    1130

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,37 @@
+export default function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">김현우</div>
+            <div className="post__date">2023.11.30</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post_delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            semper ultrices justo, a scelerisque elit pulvinar ut. Curabitur ac
+            lorem eu mauris gravida facilisis nec blandit lorem. Mauris vitae
+            quam mollis, efficitur tortor vitae, blandit mauris. Donec
+            ullamcorper nulla ipsum, in ullamcorper eros aliquet at. Morbi
+            condimentum sed lorem in aliquet. Nulla egestas convallis diam.
+            Donec magna justo, congue id ex nec, sodales porta nisi.
+            Pellentesque viverra ante ut est dictum, ut dapibus nisl ornare.
+            Phasellus non neque porttitor, ultrices nisl sed, luctus nunc. Nunc
+            tincidunt nec nulla sit amet iaculis. Nulla dapibus enim leo, in
+            aliquam purus mattis vitae. Sed a velit luctus, placerat arcu in,
+            egestas quam. In venenatis sodales ex id ultricies.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import PostList from "../../components/PostList";
+
+export default function home() {
+  return (
+    <div>
+      <Header />
+
+      <PostList />
+      <Footer />
+    </div>
+  );
+}
