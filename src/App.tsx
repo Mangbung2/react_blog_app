a7257725a6e36cc3597d4ce551addbb46a975d10
[StarIs] <[diablo8273@gmail.com]>
2023-12-08 15:16:32 +0900

    1208

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer, toast } from "react-toastify";
+import ThemeContext from "context/ThemeContext";
 
 import Router from "components/Router";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
 
   //auth를 체크하기 전에 (initialize 전)에는 loader를 띄워줌
@@ -29,10 +31,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
